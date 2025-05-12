8717e7059de5a11da51189fa62e45628ef5f2a01
Paul Applegate <paul@theapplegates.com>
2025-05-12 19:25:40 -0400

    qgpgme-qt6: bump to v1.23.2

--- Formula/qgpgme-qt6.rb
+++ Formula/qgpgme-qt6.rb
@@ -2,7 +2,7 @@ class QgpgmeQt6 < Formula
   desc "Qt6 bindings for GPGME"
   homepage "https://invent.kde.org/pim/qgpgme-qt6"
   url "https://invent.kde.org/pim/qgpgme-qt6/-/archive/v1.23.2/qgpgme-qt6-v1.23.2.tar.bz2"
-  sha256 "PUT_THE_CORRECT_SHA256_HERE"
+  sha256 "2f1f7328857a9bc57c34d42fb02c4def53b3cd62c973f6177e37e6c638756bab"
   license "LGPL-2.1-only"
 
   depends_on "cmake" => :build
