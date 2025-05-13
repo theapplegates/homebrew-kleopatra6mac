2349d17cd9cc9f5e4f5a7f795f782ca4b905ad6a
Paul <paul@theapplegates.com>
2025-05-12 21:51:38 -0400

    Update qgpgme-qt6.rb

--- Formula/qgpgme-qt6.rb
+++ Formula/qgpgme-qt6.rb
@@ -1,11 +1,9 @@
 class QgpgmeQt6 < Formula
-  desc "Qt6 bindings for GPGME"
-  homepage "https://invent.kde.org/pim/qgpgme-qt6"
-
-  url "https://invent.kde.org/pim/qgpgme-qt6/-/archive/v1.23.2/qgpgme-qt6-v1.23.2.tar.bz2"
-  sha256 "75e0928abfe1e6b8cdb4e0a84aaabc37735405ead1d4cd4f70fe33b250648df5"
-  license "LGPL-2.1-only"
-
+desc "Qt6 bindings for GPGME"
+homepage "https://invent.kde.org/pim/qgpgme-qt6"
+  url      "https://download.kde.org/stable/release-service/23.08.2/pim/qgpgme-qt6/qgpgme-qt6-v1.23.2.tar.bz2"
+sha256 "7b23420c3b104c132b4f5fd72456688cded9377167cc3b48f7dddd00031e3e13"
+license  "LGPL-2.1-only"
   depends_on "cmake" => :build
   depends_on "qt@6"
   depends_on "gpgme"
@@ -24,4 +22,3 @@ class QgpgmeQt6 < Formula
   test do
     assert_predicate include/"qgpgme_export.h", :exist?
   end
-end
