831a397277f83e585a980d2347f4e22e695cb031
Paul <paul@theapplegates.com>
2025-05-13 12:59:56 -0400

    qgpgme-qt6: fix URL, sha256 and syntax

--- Formula/qgpgme-qt6.rb
+++ Formula/qgpgme-qt6.rb
@@ -1,10 +1,10 @@
 class QgpgmeQt6 < Formula
   desc "Qt6 bindings for GPGME"
   homepage "https://invent.kde.org/pim/qgpgme-qt6"
-
   url "https://download.kde.org/stable/release-service/23.08.2/pim/qgpgme-qt6/qgpgme-qt6-v1.23.2.tar.bz2"
-  sha256 "7b23420c3b104c132b4f5fd72456688cded9377167cc3b48f7dddd00031e3e13"
+  sha256 "e0efff427b2155b09784851fa1645511ffb93629e557e887ec5dacff2fe223a0"
   license "LGPL-2.1-only"
+
   depends_on "cmake" => :build
   depends_on "qt@6"
   depends_on "gpgme"
@@ -13,16 +13,15 @@ class QgpgmeQt6 < Formula
     mkdir "build" do
       system "cmake", "..",
              "-DCMAKE_INSTALL_PREFIX=#{prefix}",
-             "-DBUILD_QT6_BINDING=ON", "-DBUILD_QT5_BINDING=OFF",
+             "-DBUILD_QT6_BINDING=ON",
+             "-DBUILD_QT5_BINDING=OFF",
              "-DBUILD_TESTING=OFF",
              *std_cmake_args
       system "make", "-j#{ENV.make_jobs}", "install"
     end
   end
-  
-  test do
-      assert_predicate include/"qgpgme_export.h", :exist?
-    end   # closes test do
-  
-  end   # closes class QgpgmeQt6 < Formula
 
+  test do
+    assert_predicate include/"qgpgme_export.h", :exist?
+  end
+end
